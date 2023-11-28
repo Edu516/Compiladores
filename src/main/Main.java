f82381e873372098986b3a8790a3b268cf1f4e6c
Eduardo <eduardo_cardoso516@outlook.com>
2023-11-28 00:27:41 -0300

    Adicionado interface
    Teste

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,7 +2,7 @@ package main;
 
 import analisadores.AnalisadorSintatico;
 import base.Automato;
-
+import Grafico.analisadorPanel;
 /**
  *
  * @author eduardo
@@ -13,22 +13,26 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Cria um automato simples para a palavra-chave "if"
         Automato automatoIf = new Automato();
-
-
-        // Cria um analisador sintático com o automato
         AnalisadorSintatico analisador = new AnalisadorSintatico(automatoIf);
 
-        // Testa algumas entradas
-        String entrada1 = "iif where if , + -- else where if else for, foreach iff"; // Inválido
-        String entrada = "(1 a a3 if else";
-        String entrada2 = "(,),[,],{,}";
-        String entrada3 = "+,-,*,/,%,--,++,<,>,<=,>=,^,!=,==,! 999 a99 99a";
-        String entrada4 = "el els else";
-        String entrada5 = "IF ELSE FOR SWITCH WHILE DO PRINT READ FOREACH";
-        analisador.validarEntrada(entrada5);
-        System.out.println(analisador.getTokens().toString());
+//        // Testa algumas entradas
+//        String entrada1 = "iif where if , + -- else where if else for, foreach iff"; // Inválido
+//        String entrada = "(1 a a3 if else";
+//        String entrada2 = "(,),[,],{,}";
+//        String entrada3 = "+,-,*,/,%,--,++,<,>,<=,>=,^,!=,==,! 999 a99 99a";
+//        String entrada4 = "el els else";
+//        String entrada5 = "IF ELSE FOR SWITCH WHILE DO PRINT READ FOREACH";
+//        analisador.validarEntrada(entrada5);
+//        System.out.println(analisador.getTokens().toString());
+        
+         // Criação e exibição da interface gráfica
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                analisadorPanel panel = new analisadorPanel();
+                panel.setVisible(true);
+            }
+        });
     }
 
 }
